6fa14f169d94f3411305bd92dba209785fd00c13
saber2pr <saber2pr@gmail.com>
2020-04-03 15:58:08 +0000

    fixed

--- click-mask.js
+++ click-mask.js
@@ -2,14 +2,14 @@
  * @Author: saber2pr
  * @Date: 2020-04-01 17:40:45
  * @Last Modified by: saber2pr
- * @Last Modified time: 2020-04-03 15:50:47
+ * @Last Modified time: 2020-04-03 15:57:41
  */
 ;(() => {
   const isMob = /Android|webOS|iPhone|iPod|BlackBerry/i.test(
     navigator.userAgent
   )
 
-  const style = `.CLICK-MASK{z-index:999;position:fixed;width:20px;height:20px;border-radius:50%;background:#a2b4cf;box-shadow:0 0 0 10px #8d8f91;animation:CLICK-MASK 0.5s ease-out;opacity:0;}@keyframes CLICK-MASK{0%{transform:scale(0);opacity:1;}100%{box-shadow:0 0 0 10px transparent;transform:scale(1);opacity:0;}}`
+  const style = `.CLICK-MASK{pointer-events:none;z-index:999;position:fixed;width:20px;height:20px;border-radius:50%;background:#a2b4cf;box-shadow:0 0 0 10px #8d8f91;animation:CLICK-MASK 0.5s ease-out;opacity:0;}@keyframes CLICK-MASK{0%{transform:scale(0);opacity:1;}100%{box-shadow:0 0 0 10px transparent;transform:scale(1);opacity:0;}}`
 
   const styler = document.createElement("style")
   styler.innerHTML = style
